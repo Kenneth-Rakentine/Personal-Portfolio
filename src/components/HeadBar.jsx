297d8a4d6aac0b0d83364bcfbe71fd1941efe1fb
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-05 13:32:32 -0400

    finished all initial/primary page content & basic layout

--- src/components/HeadBar.jsx
+++ src/components/HeadBar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function HeadBar() {
   return (
     <div className='headBarContainer'>
+        <button id='focusButton'> BOTTOM </button>
         
      </div>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,13 +5,17 @@ function Contact() {
     <div className='contactContainer'>
 
     <h1 className='contactHeadTxt'>Contact</h1>
-      
+      <div className='contactFlex'>
+      <div className='contactFloat'>
     <a href='mailto:rakentine.k@gmail.com' className='emailTxt'> Email </a>
-    <br></br>
+    </div>
+    <div className='contactFloat2'> 
     <a href='https://www.linkedin.com/in/kenneth-rakentine' className='linkedInContactTxt'>Jobs</a>
-    <br></br>
+    </div>
+    <div className='contactFloat3'>
     <a href='https://github.com/Kenneth-Rakentine' className='githubTxt'>Work</a>
-
+    </div>
+    </div>
   </div>
   )
 }

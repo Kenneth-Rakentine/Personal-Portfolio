27307b14c127ee4d512796d1582dccae41f3a7e6
Kenny Rakentine <specialistmorgenj@gmail.com>
2023-08-06 11:15:28 -0400

    added onClick events which use 'scrollTo' to take user to top and bottom of page respectively

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 function Footer() {
+
+  const footerClicker = ()=>{
+    window.scrollTo(0, 0)
+  } 
+
   return (
     <div className='footContainer'>
         <p className='footerTxt'>
         © 2023 Kenny Rakentine. All rights reserved.
-        </p> <img className='footLogo' src='https://i.ibb.co/qjyHnNc/k7-cut.png'></img>
+        </p> <img className='footLogo' src='https://i.ibb.co/qjyHnNc/k7-cut.png' onClick={footerClicker} tabIndex={0}></img>
     </div>
   )
 }
--- src/components/HeadBar.jsx
+++ src/components/HeadBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useRef } from 'react';
+
 
 
 function HeadBar() {
 
+  const clicker = ()=>{
+     window.scrollTo(0, document.body.scrollHeight);
+  }
     
   return (
-    <div className='headBarContainer'>
+    <div className='headBarContainer' onClick={clicker}>
         {/* <button id='focusButton'> BOTTOM </button> */}
         
      </div>
